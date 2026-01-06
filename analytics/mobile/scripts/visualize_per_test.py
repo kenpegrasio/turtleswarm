ba191274a59b5f0b198100facb39a61764f828e7
kenpegrasio <kenpegrasio@gmail.com>
2026-01-06 22:58:38 +0800

    feat: python scripts for graphing

---
+++ analytics/mobile/scripts/visualize_per_test.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+
+save_image = True
+files = ["../data/1.txt", "../data/2.txt", "../data/3.txt", "../data/4.txt"]
+number_of_data_per_experiment = 10
+
+
+plt.figure()
+
+for file in files:
+    with open(file, "r") as f:
+        values = [float(line.strip()) for line in f if line.strip()]
+
+    chunk_averages = [
+        sum(values[i:i + number_of_data_per_experiment]) / number_of_data_per_experiment
+        for i in range(0, len(values), number_of_data_per_experiment)
+    ]
+
+    x = list(range(1, len(chunk_averages) + 1))
+
+    plt.plot(x, chunk_averages, marker=".", label=f"{file.split('/')[-1].split('.')[0]} turtlebots")
+
+plt.xticks([1, 2, 3, 4, 5])
+plt.xlim(1, 5)
+plt.xlabel("Experiment Index")
+plt.ylabel("Average Network Latency (ms)")
+plt.title("Average Network Latency per Test")
+plt.grid(True)
+plt.legend()
+
+if save_image:
+    plt.savefig('../analysis/average_latency_per_test.png')
+
+plt.show()
---
+++ analytics/mobile/scripts/visualize_per_turtlebot.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+
+save_image = True
+files = ["../data/1.txt", "../data/2.txt", "../data/3.txt", "../data/4.txt"]
+offsets = [(0, 6), (-8, 6), (-15, 4), (-24, -2)]
+
+averages = []
+
+for file in files:
+    with open(file, "r") as f:
+        values = [float(line.strip()) for line in f if line.strip()]
+        avg = sum(values) / len(values)
+        averages.append(avg)
+
+x = list(range(1, len(files) + 1))
+
+plt.figure()
+plt.plot(x, averages, marker='o')
+plt.xlabel("Number of Turtlebots")
+plt.ylabel("Average Network Latency (ms)")
+plt.title("Average Network Latency based on Number of Turtlebots")
+plt.xticks(x)
+plt.grid(True)
+
+for xi, yi, offset in zip(x, averages, offsets):
+    plt.annotate(
+        f"{yi:.2f}",
+        (xi, yi),
+        textcoords="offset points",
+        xytext=(offset[0], offset[1]),
+        ha="center"
+    )
+
+if save_image:
+    plt.savefig("../analysis/average_latency_per_turtlebot.png")
+
+plt.show()
